21b1337b065dd140698c496fdc40b97e5eebac66
5witchkr <hob4419@gmail.com>
2022-08-16 16:11:48 +0900

    setup

---
+++ src/main/java/com/ex/soloproject5witchkr/SoloProject5witchkrApplication.java
@@ -0,0 +1,13 @@
+package com.ex.soloproject5witchkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProject5witchkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProject5witchkrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ex/soloproject5witchkr/SoloProject5witchkrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.soloproject5witchkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloProject5witchkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
